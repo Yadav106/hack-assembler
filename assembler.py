dbffebebb4ebba39fcff97e09bf8881e3e8ea305
Yadav106 <sahil.jp.yadav@gmail.com>
2025-01-13 14:38:51 +0530

    done with C instr

--- assembler.py
+++ assembler.py
@@ -31,6 +31,58 @@ class Assembler:
             "THIS": 3,
             "THAT": 4,
         }
+
+        self.compbits = {
+            "0": "0101010",
+            "1": "0111111",
+            "-1": "0111010",
+            "D": "0001100",
+            "A": "0110000",
+            "M": "1110000",
+            "!D": "0001101",
+            "!A": "0110001",
+            "!M": "1110001",
+            "-D": "0001111",
+            "-A": "0110011",
+            "-M": "1110011",
+            "D+1": "0011111",
+            "A+1": "0110111",
+            "M+1": "1110111",
+            "D-1": "0001110",
+            "A-1": "0110010",
+            "M-1": "1110010",
+            "D+A": "0000010",
+            "D+M": "1000010",
+            "D-A": "0010011",
+            "D-M": "1010011",
+            "A-D": "0000111",
+            "M-D": "1000111",
+            "D&A": "0000000",
+            "D&M": "1000000",
+            "D|A": "0010101",
+            "D|M": "1010101"
+        }
+        self.destbits = {
+            "-1": "000",
+            "M": "001",
+            "D": "010",
+            "MD": "011",
+            "A": "100",
+            "AM": "101",
+            "AD": "110",
+            "AMD": "111"
+        }
+        self.jumpbits = {
+            "-1": "000",
+            "JGT": "001",
+            "JEQ": "010",
+            "JGE": "011",
+            "JLT": "100",
+            "JNE": "101",
+            "JLE": "110",
+            "JMP": "111"
+        }
+
         self.machine_code = []
 
     def assemble(self):
@@ -91,7 +143,24 @@ class Assembler:
         self.machine_code.append(a_instr)
 
     def parse_C_instr(self, ip):
-        pass
+        jump = "-1"
+        dest = "-1"
+
+        jump_re = re.split(";", ip)
+        if len(jump_re) == 2:
+            jump = jump_re[1]
+            ip = jump_re[0]
+
+        dest_re = re.split("=", ip)
+        if len(dest_re) == 2:
+            dest = dest_re[0]
+            ip = dest_re[1]
+
+        comp = ip
+
+        c_instr = "111" + str(self.compbits.get(comp)) + str(self.destbits.get(dest)) + str(self.jumpbits.get(jump))
+        self.machine_code.append((c_instr))
+
 
     def fill_symbol_table(self):
         ipLines = self.input.splitlines()
